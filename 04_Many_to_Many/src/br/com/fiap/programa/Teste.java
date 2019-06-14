4d2fe077bddb41a2301f283e80d03823ce4835e0
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-14 19:41:08 -0300

    many to many com JPA

---
+++ 04_Many_to_Many/src/br/com/fiap/programa/Teste.java
@@ -0,0 +1,56 @@
+package br.com.fiap.programa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.fiap.entity.Tarefa;
+import br.com.fiap.entity.Funcionario;
+import br.com.fiap.helper.Helper;
+
+public class Teste {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Many_to_Many");
+		EntityManager em = emf.createEntityManager();
+
+		incluirFuncionario(em);
+		listarFuncionarios(em);
+		buscarFuncionario(em, "2000");
+		em.close();
+	}
+
+	private static void incluirFuncionario(EntityManager em) {
+		Helper dao = new Helper(em);
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setMatricula("8000");
+		funcionario.setNome("Asdf");
+		Tarefa tarefa = new Tarefa();
+		tarefa.setDescricao("Teste Unitário");
+		tarefa.setDuracao(100);
+		tarefa.getFuncionarios().add(funcionario);
+		funcionario.getTarefas().add(tarefa);
+		try {
+			dao.salvar(funcionario);
+			System.out.println("Funcionario OK");
+		} catch (Exception e) {
+			System.out.println("ERRO ===>> " + e.getMessage());
+		}
+	}
+
+	private static void listarFuncionarios(EntityManager em) {
+		Helper dao = new Helper(em);
+		List<Funcionario> funcionarios = dao.listarTodos();
+		for (Funcionario funcionario : funcionarios) {
+			System.out.println(funcionario.getMatricula() + ": " + funcionario.getNome());
+		}
+	}
+
+	private static void buscarFuncionario(EntityManager em, String matricula) {
+		Helper dao = new Helper(em);
+		Funcionario f = dao.buscarFuncionario(matricula);
+		System.out.println(f.getMatricula() + ": " + f.getNome());
+	}
+}
